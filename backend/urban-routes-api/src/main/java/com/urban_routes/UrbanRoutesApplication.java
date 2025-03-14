0ad4e9ed312df300144d8ce8985bd011b1c431cd
Brian <brian9work@gmail.com>
2025-03-13 19:07:32 -0600

    Initialization of the backend aplication by Springboot

---
+++ backend/urban-routes-api/src/main/java/com/urban_routes/UrbanRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.urban_routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanRoutesApplication.class, args);
+	}
+
+}
---
+++ backend/urban-routes-api/src/test/java/com/urban_routes/UrbanRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urban_routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
