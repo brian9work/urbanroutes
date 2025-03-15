72022061b1c2f177cb1f1862d3e579d00947c1cf
Brian <brian9work@gmail.com>
2025-03-14 23:17:43 -0600

    Add more imagens

--- urban-routes-mobile-app/constants/images.js
+++ urban-routes-mobile-app/constants/images.js
@@ -2,12 +2,16 @@ import transport1 from '../assets/images/version/transport1.jpg';
 import transport2 from '../assets/images/version/transport2.jpg';
 import transport3 from '../assets/images/version/transport3.jpg';
 import transport4 from '../assets/images/version/transport4.jpg';
+import transport5 from '../assets/images/version/transport5.jpg';
+import transport6 from '../assets/images/version/transport6.jpg';
 
 export default images = {
     version: {
         transport1: transport1,
         transport2: transport2,
         transport3: transport3,
-        transport4: transport4
+        transport4: transport4,
+        transport5: transport5,
+        transport6: transport6,
     }
 }
