57c8aebae8c056f5de96dcb7519ad04837ed92d0
Brian <brian9work@gmail.com>
2025-03-17 09:57:28 -0600

    Creation of file catalogs controller

--- backend/urban-routes-api/src/main/java/com/urbanroutes/dev/controller/CatalogsController.java
+++ backend/urban-routes-api/src/main/java/com/urbanroutes/dev/controller/CatalogsController.java
@@ -1,4 +1,42 @@
 package com.urbanroutes.dev.controller;
 
+import com.urbanroutes.dev.models.catalogs.*;
+import com.urbanroutes.dev.services.CatalogsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cat")
 public class CatalogsController {
+    @Autowired
+    private CatalogsService catalogsService;
+
+    @GetMapping("/degree")
+    public List<CatDegreeModel> getDegree() {
+        return this.catalogsService.getAllDegree();
+    }
+
+    @GetMapping("/faculty")
+    public List<CatFacultyModel> getFaculty() {
+        return this.catalogsService.getAllFaculty();
+    }
+
+    @GetMapping("/municipality")
+    public List<CatMunicipalitiesModel> getMunicipalities() {
+        return this.catalogsService.getAllMunicipalities();
+    }
+
+    @GetMapping("/typeTransport")
+    public List<CatTypeOfTransportModel> getTypeOfTransport() {
+        return this.catalogsService.getAllTypeOfTransport();
+    }
+
+    @GetMapping("/lineTransport")
+    public List<CatLineOfTransportModel> getLineOfTransport() {
+        return this.catalogsService.getAllLineOfTransport();
+    }
 }
