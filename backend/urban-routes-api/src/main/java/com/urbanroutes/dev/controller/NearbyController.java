0402456b69a4e993aca85263c4dc8d23ec880ea8
Brian <brian9work@gmail.com>
2025-03-17 16:49:48 -0600

    Modification of files of Controller

--- backend/urban-routes-api/src/main/java/com/urbanroutes/dev/controller/NearbyController.java
+++ backend/urban-routes-api/src/main/java/com/urbanroutes/dev/controller/NearbyController.java
@@ -1,4 +1,32 @@
 package com.urbanroutes.dev.controller;
 
+import com.urbanroutes.dev.DTO.NearbyStopDTO;
+import com.urbanroutes.dev.DTO.TransportDTO;
+import com.urbanroutes.dev.services.NearbyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/nearby")
 public class NearbyController {
+    @Autowired
+    private NearbyService nearbyService;
+
+    @GetMapping
+    public List<NearbyStopDTO> getNearbyStops(
+            @RequestParam(defaultValue = "0") double latitude,
+            @RequestParam(defaultValue = "0") double longitude,
+            @RequestParam(defaultValue = "0") double distance )
+    {
+        return this.nearbyService.getNearbyStops(latitude, longitude, distance);
+    }
+
+
+    @GetMapping("/{idStop}")
+    public List<TransportDTO> getNearbyTransportForStop(@PathVariable Long idStop )
+    {
+        return this.nearbyService.getNearbyTransportForStop(idStop);
+    }
 }
--- backend/urban-routes-api/src/main/java/com/urbanroutes/dev/controller/TransportController.java
+++ backend/urban-routes-api/src/main/java/com/urbanroutes/dev/controller/TransportController.java
@@ -1,4 +1,27 @@
 package com.urbanroutes.dev.controller;
 
+import com.urbanroutes.dev.DTO.TransportDTO;
+import com.urbanroutes.dev.services.NearbyService;
+import com.urbanroutes.dev.services.TransportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transport")
 public class TransportController {
+    @Autowired
+    private TransportService transportService;
+
+
+    @GetMapping("/{idTransport}")
+    public TransportDTO getTransport(@PathVariable Long idTransport )
+    {
+        return this.transportService.getTransport(idTransport);
+    }
+
 }
