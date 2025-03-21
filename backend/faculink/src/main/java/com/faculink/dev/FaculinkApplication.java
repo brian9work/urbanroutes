5490547a1bc6e7b4233fb70f4b8b4dcc0789b5b2
Brian <brian9work@gmail.com>
2025-03-21 11:04:19 -0600

    Creation of a new sistem backend with Gradle

---
+++ backend/faculink/src/main/java/com/faculink/dev/FaculinkApplication.java
@@ -0,0 +1,13 @@
+package com.faculink.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaculinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaculinkApplication.class, args);
+	}
+
+}
---
+++ backend/faculink/src/test/java/com/api/faculink/FaculinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.faculink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
