068a07331b5877c8fbfcf83afc763c0557c10984
Brian <brian9work@gmail.com>
2025-03-24 18:44:05 -0600

    modfication of colors

--- urban-routes-mobile-app/tailwind.config.js
+++ urban-routes-mobile-app/tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     "./App.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
+    "./screens/*.{js,jsx,ts,tsx}",
+    "./constants/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./app/*.{js,jsx,ts,tsx}"
  ],
@@ -15,11 +17,11 @@ module.exports = {
        colors: {
           common: {
             blue: "#007BFF",
-            red: "#DC354580",
-            green: "#28A74580",
-            yellow: "#FFC10780",
-            purple: "#6F42C180",
-            orange: "#FD7E1480",
+            red: "#DC3545",
+            green: "#28A745",
+            yellow: "#FFC107",
+            purple: "#6F42C1",
+            orange: "#FD7E14",
             black: "#212529",
             white: "#F8F9FA",
           },
