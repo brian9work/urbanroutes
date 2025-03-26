0d3a6bc3eb65ec076e3e5d7a13a467d08de7ec8c
Brian <brian9work@gmail.com>
2025-03-25 18:40:21 -0600

    Modification in the design of the icons

--- urban-routes-mobile-app/constants/Icons.js
+++ urban-routes-mobile-app/constants/Icons.js
@@ -5,7 +5,7 @@ export const WalkIcon = () => {
     return (
             <FontAwesome5 name="walking"
                 size={20}
-                className=" bg-common-blue/15 p-3 rounded-full"
+                className=" bg-common-blue/15 p-2 rounded-full"
                 color="#007BFF"
             />
     )
@@ -16,7 +16,7 @@ export const StopIcon = () => {
             name="bus-stop-uncovered"
             size={24}
             color="#212529"
-            className="p-3 bg-theme-black/10 rounded-full"
+            className="p-2 bg-theme-black/10 rounded-full"
         />
     )
 }
@@ -25,13 +25,13 @@ export const TransportIcon = () => {
         <FontAwesome6 name="bus"
             size={26}
             color="#212529"
-            className="p-3  rounded-full"
+            className="p-2  rounded-full"
         />
     )
 }
 export const MoneyIcon = () => {
     return (
-        <View className=" bg-common-green/15 p-3 rounded-full">
+        <View className=" bg-common-green/15 p-2 rounded-full">
             <MaterialIcons name="attach-money"
                 size={22}
                 color="#28A745"
@@ -41,7 +41,7 @@ export const MoneyIcon = () => {
 }
 export const ClockIcon = () => {
     return (
-        <View className=" bg-common-blue/15 p-3 rounded-full">
+        <View className=" bg-common-blue/15 p-2 rounded-full">
             <MaterialCommunityIcons name="clock-time-three-outline"
                 size={22}
                 color="#007BFF"
@@ -51,7 +51,7 @@ export const ClockIcon = () => {
 }
 export const RouteIcon = () => {
     return (
-        <View className=" bg-yellow-200/30 p-3 rounded-full">
+        <View className=" bg-yellow-200/30 p-2 rounded-full">
             <FontAwesome5 name="route"
                 size={22}
                 color="#F7BE00"
