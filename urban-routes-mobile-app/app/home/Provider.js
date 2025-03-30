bec06832673ee14bebc31880fd6840857bf673e4
Brian <brian9work@gmail.com>
2025-03-29 22:59:20 -0600

    Creation of new providers

--- urban-routes-mobile-app/app/home/Provider.js
+++ urban-routes-mobile-app/app/home/Provider.js
@@ -4,13 +4,29 @@ import { ContextHome } from "./Context";
 export default function Provider ({ children }) {
    const selectedMain = useState("NearbyStops");
    const nearbyStopsData = useState([]);
-   const transportsForStopData = useState([]);
+   const transportsForStopData = useState(["",""]);
    const selectedIdStop = useState(0);
    const selectedIdtransport = useState(0);
    const infoOfTransport = useState({route:[]});
    const activeMenu = useState(true);
    const activeMenuSearch = useState(false);
    const selectedDestination = useState(0);
+   const facultiesData = useState([]);
+
+   const location = useState({
+      latitude: 19.41514082532041,
+      longitude: -98.14024764753933,
+      distance: 1000,
+      accuracy: 0,
+   })
+
+   const facultySelected = useState({
+      id: 0,
+      name: "Seleccionar Facultad",
+      latitude: 0,
+      longitude: 0,
+      distance: 0,
+   })
 
    const notificationValue= useState({
       value: false,
@@ -36,9 +52,14 @@ export default function Provider ({ children }) {
          activeMenu,
          activeMenuSearch,
          selectedDestination,
+         facultiesData,
+
+         location,
 
          notificationValue,
-         notificationValueChangue
+         notificationValueChangue,
+
+         facultySelected
       }}>
          {children}
       </ContextHome.Provider>
