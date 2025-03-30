e6b38ec5f2ce3f052cf4349cc8eda25ba3194414
Brian <brian9work@gmail.com>
2025-03-29 22:59:41 -0600

    Build configuration

---
+++ urban-routes-mobile-app/commands/.cmd
@@ -0,0 +1,11 @@
+eas build -p android --profile preview // Generate APK
+npm install -g eas-cli // Install EAS CLI
+eas login // Login to EAS
+eas whoami // Check if logged in
+eas build:configure // Configure EAS for the project
+npx create-expo-app@latest aplication --template blank // Create new Expo app
+git update-index --assume-unchanged file // Ignore changes in app.json
+git update-index --no-assume-unchanged file // Stop ignoring changes in app.json
+
+
+    
