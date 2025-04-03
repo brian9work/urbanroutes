f4ff3721def9de15aa6fa8cbaa4ccb5c49ef4e57
Brian <brian9work@gmail.com>
2025-04-02 22:50:50 -0600

    Feature for take the  user location

---
+++ urban-routes-mobile-app/screens/home/components/search/MyLocation.js
@@ -0,0 +1,56 @@
+import { View, Text, Image, Alert } from 'react-native'
+import React, { useContext, useEffect } from 'react'
+import *  as Location from 'expo-location';
+import { ContextHome } from '../../../../app/home/Context';
+import { Marker } from 'react-native-maps';
+import images from '../../../../constants/images';
+
+export default function MyLocation() {
+   const { location, notificationValueChangue } = useContext(ContextHome);
+   const [locationState, setLocationState] = location;
+
+   const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+         console.warn("Permission to access location was denied")
+         Alert.alert("Permisos de ubicación", "Para poder usar la aplicación es necesario habilitar los permisos de ubicación")
+         notificationValueChangue("No se pudo obtener la ubicación")
+         return false
+      }
+      let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
+      const current = {
+         latitude: location.coords.latitude,
+         longitude: location.coords.longitude,
+         distance: 1000,
+         accuracy: location.coords.accuracy,
+      }
+      console.log("Location: ", current)
+      setLocationState({
+         latitude: current.latitude,
+         longitude: current.longitude,
+         distance: current.distance,
+         accuracy: current.accuracy,
+      })
+   }
+
+   useEffect(() => {
+      getLocation()
+   }, [])
+
+   return (
+      <Marker
+         coordinate={{
+            latitude: locationState.latitude,
+            longitude: locationState.longitude,
+         }}
+         title='Mi ubicación'
+         // icon={images.icons.location}
+      >
+         <Image
+            source={images.icons.location}
+            className="object-cover object-center"
+            style={{ width: 110, height: 110 }}
+         />
+      </Marker>
+   )
+}
