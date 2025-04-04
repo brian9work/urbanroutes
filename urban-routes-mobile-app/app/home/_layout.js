3245bcfece890717ba4e8796e86119285836b8d2
Brian <brian9work@gmail.com>
2025-04-04 08:28:39 -0600

    Creation of a context global

--- urban-routes-mobile-app/app/home/_layout.js
+++ urban-routes-mobile-app/app/home/_layout.js
@@ -4,15 +4,17 @@ import { StatusBar } from "expo-status-bar";
 import Provider from "./Provider";
 import Search from "../../screens/home/Search";
 import Map from "../../screens/home/Map";
+import { Notification } from "../../components/common/Notifications";
 
 export default function Layout() {
-  return (
-    <Provider>
-      <View className=" relative ">
-        {/* <Map /> */}
-        <Slot />
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
-  );
+   return (
+      <Provider>
+         <View className=" relative ">
+            {/* <Map /> */}
+            <Notification />
+            <Slot />
+            <StatusBar style="auto" />
+         </View>
+      </Provider>
+   );
 }
