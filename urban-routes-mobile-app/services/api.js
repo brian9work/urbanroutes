72f3593fc20197c9774102d35b764003f6402392
Brian <brian9work@gmail.com>
2025-04-05 15:37:40 -0600

    Correction in the endpoint

--- urban-routes-mobile-app/services/api.js
+++ urban-routes-mobile-app/services/api.js
@@ -10,28 +10,28 @@ const Api ={
         verifyVersion: () =>  `https://brian9work.github.io/static/Api/version4.json`,
     },
     nearby: {
-        stops: (originStatic, latitude, longitude, distance) => {
+        stops: (originStatic="https://a417-3-144-96-83.ngrok-free.app/api", latitude, longitude, distance) => {
             return `${originStatic}/nearby?latitude=${latitude}&longitude=${longitude}&distance=${distance}`;
         },
-        selectedStop: (originStatic, idStop) => {
+        selectedStop: (originStatic="https://a417-3-144-96-83.ngrok-free.app/api", idStop) => {
             return `${originStatic}/nearby/${idStop}`;
         }
     },
     transport: {
-        getTransport : (originStatic, idTransport) => {
+        getTransport : (originStatic="https://a417-3-144-96-83.ngrok-free.app/api", idTransport) => {
             return `${originStatic}/transport/${idTransport}`;
         },
-        getTransportBeetwenTwoStops : (originStatic, idTransport, originStop, destinationStop) => {
+        getTransportBeetwenTwoStops : (originStatic="https://a417-3-144-96-83.ngrok-free.app/api", idTransport, originStop, destinationStop) => {
             return `${originStatic}/transport?idTransport=${idTransport}&originStop=${originStop}&destinationStop=${destinationStop}`;
         }
     },
     catalogs: {
-        getFaculties: (originStatic, ) => {
+        getFaculties: (originStatic="https://a417-3-144-96-83.ngrok-free.app/api", ) => {
             return `${originStatic}/cat/faculty`;
         }
     },
     stopRoutes: {
-        getStopRouteFromAndTo: (originStatic, stopFrom, stopTo) => {
+        getStopRouteFromAndTo: (originStatic="https://a417-3-144-96-83.ngrok-free.app/api", stopFrom, stopTo) => {
             return `${originStatic}/sr?stopFrom=${stopFrom}&stopTo=${stopTo}`;
         }
     },
