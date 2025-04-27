a9ab60a2adbaef7db85d148c091d8470ccdbef0f
Brian <brian9work@gmail.com>
2025-04-27 15:52:58 -0600

    Changue of the message of load

--- urban-routes-mobile-app/screens/home/Map.js
+++ urban-routes-mobile-app/screens/home/Map.js
@@ -1,11 +1,8 @@
-import { View, Text, Image, Alert } from 'react-native'
+import { View } from 'react-native'
 import React, { useContext, useEffect } from 'react'
-import MapView, { Marker, Polygon, Polyline, PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import Dim from '../../constants/dimensions';
 import ContextHome from '../../app/home/Context';
-import Empty from '../../components/common/Empty';
-import dataMain from './components/main/mainData'
-import images from '../../constants/images';
 import MyLocation from './components/map/MyLocation';
 import NearbyStops from './components/map/NearbyStops';
 import NearbyStopsForFaculty from './components/map/NearbyStopsForFaculty';
@@ -13,16 +10,11 @@ import FacultyBorder from './components/map/FacultyBorder';
 import RouteSelected from './components/map/RouteSelected';
 import SelectedStopMarker from './components/map/SelectedStopMarker';
 import SearchPublicTransportMarker from './components/map/SearchPublicTransportMarker';
+import MapStyle from './components/map/MapStyle';
 
-const MapStyle = [
-   { "featureType": "administrative", "elementType": "geometry", "stylers": [{ "visibility": "off" }] },
-   { "featureType": "poi", "stylers": [{ "visibility": "off" }] },
-   { "featureType": "road", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] },
-   { "featureType": "transit", "stylers": [{ "visibility": "off" }] }
-]
 
 export default function Map() {
-   const { nearbyStopsData, infoOfTransport, facultiesData } = useContext(ContextHome);
+   const { nearbyStopsData, infoOfTransport, notificationValueChangue, originLocation, facultiesData } = useContext(ContextHome);
    const [nearbyStopsDataState, setNearbyStopsDataState] = nearbyStopsData;
    const [dataTransportState, setDataTransportState] = infoOfTransport;
 
@@ -30,21 +22,10 @@ export default function Map() {
 
    useEffect(() => {
       if (nearbyStopsDataState.length === 0) {
-         console.warn("No hay bases cercanas")
+         notificationValueChangue("Buscando bases de stops cercanas.", "info")
       }
    }, [nearbyStopsDataState, dataTransportState])
 
-
-   if (nearbyStopsDataState.length === 0) {
-      return (
-         <>
-            <Empty
-               message='Lo sentimos hay bases cercanas a su ubicación'
-               description='Intente moverse a otra ubicación o ampliar el radio de búsqueda'
-            />
-         </>
-      )
-   }
 
    return (
       <View style={{ width: '100%', height: '100%', position: 'absolute', left: 0, top: 0 }}>
@@ -53,10 +34,10 @@ export default function Map() {
                provider={PROVIDER_GOOGLE}
                style={{ width: dimensions.width, height: dimensions.height * 1.1 }}
                initialRegion={{
-                  latitude: 19.415168098195434,
-                  // latitude: locationState.latitude,
-                  longitude: -98.14026486519624,
-                  // longitude: locationState.longitude,
+                  // latitude: 19.415168098195434,
+                  // longitude: -98.14026486519624,
+                  latitude: originLocation[0].latitude,
+                  longitude: originLocation[0].longitude,
                   latitudeDelta: 0.01,
                   longitudeDelta: 0.01
                }}
@@ -64,13 +45,21 @@ export default function Map() {
                showsUserLocation={true}
                showsMyLocationButton={true}
             >
-               <MyLocation />
-               <RouteSelected />
-               <NearbyStops />
-               <NearbyStopsForFaculty />
-               <FacultyBorder />
-               <SelectedStopMarker />
-               <SearchPublicTransportMarker />
+               {
+                  nearbyStopsDataState.length === 0 ? (
+                     <></>
+                  ) : (
+                     <>
+                        <MyLocation />
+                        <RouteSelected />
+                        <NearbyStops />
+                        <NearbyStopsForFaculty />
+                        <FacultyBorder />
+                        <SelectedStopMarker />
+                        <SearchPublicTransportMarker />
+                     </>
+                  )
+               }
             </MapView>
          </View>
       </View>
---
+++ urban-routes-mobile-app/screens/home/components/map/MapStyle.js
@@ -0,0 +1,8 @@
+const MapStyle = [
+    { "featureType": "administrative", "elementType": "geometry", "stylers": [{ "visibility": "off" }] },
+    { "featureType": "poi", "stylers": [{ "visibility": "off" }] },
+    { "featureType": "road", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] },
+    { "featureType": "transit", "stylers": [{ "visibility": "off" }] }
+]
+
+export default MapStyle;
